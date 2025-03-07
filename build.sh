4d4cbded9115a49bb6e8da0e9b1f4f7ce2099c5b
Georgi Murdzheff <120983342+murdzheff@users.noreply.github.com>
2025-03-07 15:01:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ dnf copr enable peterwu/iosevka
 
 
 # this installs a package from fedora repos
-dnf install -y tmux tailscale curl wayvnc
+dnf install -y tmux tailscale curl wayvnc xfreerdp
 
 #install iosevka
 curl -sL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip -o /tmp/Iosevka.zip
