72be9f840d765f7da999e1e582fb1a1fd95c4f01
Georgi Murdzheff <120983342+murdzheff@users.noreply.github.com>
2025-07-24 09:09:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,86 +8,18 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-curl -s -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/centos/9/tailscale.repo
-
-
-dnf install @development-tools -y
+#curl -s -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/centos/9/tailscale.repo
+dnf copr enable aleasto/waydroid -y
 
 
 packages=(
   tmux
-  tailscale
   curl
   gcc
   xfreerdp
-  rofi-wayland
-  kitty
-  xdg-desktop-portal-hyprland
-  waybar
-  xorg-x11-server-Xwayland
-  headsetcontrol
-  polkit
-  xfce-polkit
-  xdg-user-dirs
-  dbus-tools
-  dbus-daemon
-  wl-clipboard
-  pavucontrol
-  playerctl
-  qt5-qtwayland
-  qt6-qtwayland
-  vulkan-validation-layers
-  vulkan-tools
-  google-noto-emoji-fonts
-  gnome-disk-utility
-  ddcutil
-  openssl
-  vim
-  just
-  alsa-firmware
-  p7zip
-  distrobox
-  dunst
-  lxappearance
-  gvfs-smb
-  smbclient
-  wireplumber
-  pipewire
-  pamixer
-  pulseaudio-utils
-  NetworkManager-openvpn
-  NetworkManager-openvpn-gnome
-  NetworkManager-openconnect
-  NetworkManager-openconnect-gnome
-  bluez
-  bluez-tools
-  blueman
-  firewall-config
-  thunar
-  thunar-archive-plugin 
-  thunar-volman
-  xarchiver
-  imv
-  p7zip
-  gvfs-mtp
-  gvfs-gphoto2
-  gvfs-smb
-  gvfs-nfs
-  gvfs-fuse
-  gvfs-archive
-  android-tools
-  slurp
-  grim
-  wlr-randr
-  brightnessctl
-  kanshi
-  fontawesome-fonts-all
   syncthing
   helix
-  gnome-themes-extra
-  gnome-icon-theme
-  paper-icon-theme
-  breeze-icon-theme 
+  waydroid
 )
 
 for package in "${packages[@]}"; do
@@ -110,7 +42,7 @@ cp -r /tmp/.config /etc/custom-configs
 
 #scp -r /etc/skel/.config/* /etc/xdg/
 #scp -r /etc/skel/.config/hypr/hyprland.conf /usr/share/hyprland/
-
+dnf copr disable aleasto/waydroid -y
 
 # Use a COPR Example:
 #
